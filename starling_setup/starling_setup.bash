0f625a30501712d3ad13741f609761ddc4780980
Ryan Kuederle <ryankuederle@icloud.com>
2024-04-12 09:03:27 -0400

    Added some setup scripts for Starling

---
+++ starling_setup/starling_setup.bash
@@ -0,0 +1,17 @@
+#!/bin.bash
+
+# Remove voxl aeroprint service and copy it from this directory.
+cp ~/aeroprint/starling_setup/voxl-aeroprint-start.service /etc/systemd/system/voxl-aeroprint-start.service
+cp ~/aeroprint/starling_setup/startup_command.bash /etc/aeroprint/startup.bash
+# Set appropriate services
+sudo systemctl enable voxl-aeroprint-start
+
+if ["$STARLING_MODE" ==  "SIM"]
+then
+  sudo systemctl disable voxl-px4;
+  sudo systemctl disable voxl-qvio-server;
+else if ["$STARLING_MODE" == "FLIGHT"]
+then
+  sudo systemctl enable voxl-px4;
+  sudo systemctl enable voxl-qvio-server;
+fi
---
+++ starling_setup/startup_commands.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+source /opt/ros/foxy/setup.bash
+echo "2" | voxl-configure-microdds
+source /opt/ros/foxy/mpa_to_ros2/install/setup.bash
+ros2 run voxl_mpa_to_ros2 voxl_mpa_to_ros2 &
