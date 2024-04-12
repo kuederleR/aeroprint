51dc5fbbeaba28cd43186a0750756373baff57c0
Ryan Kuederle <ryankuederle@icloud.com>
2024-04-12 10:39:52 -0400

    Fixed some more bugs.

--- starling_setup/startup_commands.bash
+++ starling_setup/startup_commands.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-# source /opt/ros/foxy/setup.bash
 echo "2" | voxl-configure-microdds
-# source /opt/ros/foxy/mpa_to_ros2/install/setup.bash
-ros2 run voxl_mpa_to_ros2 voxl_mpa_to_ros2_node &
+source /opt/ros/foxy/setup.bash
+source /opt/ros/foxy/mpa_to_ros2/install/local_setup.bash
+ros2 run voxl_mpa_to_ros2 voxl_mpa_to_ros2_node
