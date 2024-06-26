f5107c92ce353e3f9b930d4c577bf850256f74ed
Ryan Kuederle <ryan@rkuederle.com>
2024-06-26 11:15:55 -0400

    chore: Add .venv to .gitignore and implement transfer learning logic in PyQt6 application

--- src/data_management/data_management/transfer_learning.py
+++ src/data_management/data_management/transfer_learning.py
@@ -1,4 +1,6 @@
 from tensorflow.keras.applications import EfficientNetV2L
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, GlobalAveragePooling2D, Dense, Dropout
 
 model = EfficientNetV2L(weights='imagenet', 
                         include_top=False,
@@ -6,3 +8,29 @@ model = EfficientNetV2L(weights='imagenet',
 
 for layer in model.layers:
     layer.trainable = False
+
+my_model = Sequential([model, 
+                      Conv2D(1024, 3, 1, activation='relu'),
+                      GlobalAveragePooling2D(),
+                      Dense(1024, activation='relu'), 
+                      Dropout(0.2),
+                      Dense(1024, activation='relu'), 
+                      Dropout(0.2),
+                      Dense(1, activation='sigmoid')])
+# For more than two classifications, change the last Dense layer to the number of classes and the activation to 'softmax'
+import os
+try: 
+   os.mkdir('images')
+except: pass
+
+try:
+   os.makedirs(os.path.join('images', 'cats'))
+except: pass
+
+try:
+   os.makedirs(os.path.join('images', 'dpgs'))
+except: pass
+
+
+
+
